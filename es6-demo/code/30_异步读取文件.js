f829300df0abb7fa2d67b44c8e36e8cdca7af414
utopianpuppy <734507502@qq.com>
2019-07-23 17:30:14 +0800

    company 2019-7-23

---
+++ es6-demo/code/30_异步读取文件.js
@@ -0,0 +1,58 @@
+let fs = require('fs');
+
+// fs.readFile('./a.txt', (err, data) => {
+//     console.log(data.toString())
+// });
+//
+// fs.readFile('./b.txt', (err, data) => {
+//     console.log(data.toString())
+// });
+//
+// fs.readFile('./c.txt', (err, data) => {
+//     console.log(data.toString())
+// });
+
+
+function readFile(path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, (err, data) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(data.toString());
+            }
+            // console.log(data.toString())
+        });
+    })
+}
+
+//解决方案1 Promise()
+// readFile('./a.txt').then((data) => {
+//     console.log(data);
+//     return readFile('./b.txt')
+// }).then((data) => {
+//     console.log(data);
+//     return readFile('./c.txt')
+// }).then((data) => {
+//     console.log(data);
+// });
+
+//解决方案2 generator
+function* gen() {
+    yield readFile('./a.txt');
+    yield readFile('./b.txt');
+    yield readFile('./c.txt');
+}
+
+let it = gen();
+// console.log(it.next()); //{ value: Promise { <pending> }, done: false }
+
+it.next().value.then((data) => {
+    console.log(data);
+    return it.next().value
+}).then((data) => {
+    console.log(data);
+    return it.next().value
+}).then((data) => {
+    console.log(data);
+});
